4d0f31880482762f6d5c0ce28c078eaf972f9d0f
yusufozby <Ozbay.yusuf02@gmail.com>
2023-03-03 10:43:04 +0300

    ewqewq

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld;
+
+class Program {
+
+    static void Main(String[] args) {
+        Console.WriteLine("Enter a course name");
+        string courseName = Console.ReadLine();
+        Console.WriteLine("Welcome to "+courseName);
+    }
+}
